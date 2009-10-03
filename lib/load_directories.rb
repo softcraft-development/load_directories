fb1ec17cfaf92ca925958d876cfddc29f1563d43
Craig Walker <github@softcraft.ca>
2009-10-03 17:03:06 -0600

    Added load_directories.rb

--- lib/load_directories.rb
+++ lib/load_directories.rb
@@ -1,6 +1,15 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+require 'set'
 
-module LoadDirectories
-  VERSION = '0.0.1'
+def load_directories( root )
+  subdirectories = Set.new([File.expand_path(root)])
+  Dir[root + "/**"].each do |f|
+    if File.directory? f
+      fullpath = File.expand_path f
+      subdirectories << fullpath
+    end
+  end
+  existing = Set.new($LOAD_PATH)
+  to_add = subdirectories-existing
+  to_add.each do |d| $LOAD_PATH << d end
+  to_add
 end
